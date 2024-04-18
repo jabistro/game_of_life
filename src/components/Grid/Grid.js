1368bc947f36b16ff11c2483b29f37c4bec5e8c4
John Allan Hinds <jabistro@gmail.com>
2024-04-18 17:05:47 -0400

    resetGrid & runGame initiated

--- src/components/Grid/Grid.js
+++ src/components/Grid/Grid.js
@@ -1,7 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Grid.css';
+import { resetGrid } from '../../utils/resetGrid';
+
+const numRows = 40;
+const numCols = 40;
 
 const Grid = () => {
+  const [grid, setGrid] = useState(() => {
+    return resetGrid();
+  })
+  const [running, setRunning] = useState(false);
+  const runningRef = useRef(running);
+  runningRef.current = running;
+
   return (
     <div>Grid</div>
   )
---
+++ src/utils/resetGrid.js
@@ -0,0 +1,6 @@
+export function resetGrid() {
+  const rows = [];
+  for (let i = 0; i < numRows; i++) {
+    rows.push(Array.from(Array(numCols), () => 0));
+  }
+}
