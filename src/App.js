827a4c42009141fa47aee667501a09ff1650a32a
John Allan Hinds <jabistro@gmail.com>
2024-04-18 16:29:21 -0400

    coordinates.js

--- src/App.js
+++ src/App.js
@@ -1,8 +1,10 @@
 import './App.css';
+import Grid from './components/Grid/Grid.js';
 
 function App() {
   return (
     <div className="App">
+      <Grid />
     </div>
   );
 }
---
+++ src/components/Grid/Grid.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import './Grid.css';
+
+const Grid = () => {
+  return (
+    <div>Grid</div>
+  )
+}
+
+export default Grid
